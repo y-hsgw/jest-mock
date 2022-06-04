1c67d22b1d8f5aef449e9ae02da366f722aaddc4
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2022-06-04 15:23:58 +0900

    spyOnのテスト例

---
+++ src/__tests__/jest-spyOn.test.ts
@@ -0,0 +1,45 @@
+import * as calc from "../modules/calc";
+
+describe("jest.spuOn()", () => {
+  it("Math random() return 1", () => {
+    /** Math.random()をコールした時の戻り値は1に固定
+     * Math,randomは0から1未満の数値を返すので、本来1はありえない
+     */
+    const spy = jest.spyOn(Math, "random").mockReturnValue(1);
+    /** Math.random()が1であることを確認 */
+    expect(Math.random()).toBe(1);
+    /** jest.spyOn によって作成されたモックをオリジナルのものへ戻す。
+     * これがないと他の箇所でMath.randomをコールするとエラーになる
+     */
+    spy.mockRestore();
+  });
+
+  it("appropriate Math random() return", () => {
+    /** 戻り値が1以外になったことを確認 */
+    expect(Math.random()).not.toBe(1);
+    /** Math.random()が1未満であることを確認 */
+    expect(Math.random()).toBeLessThan(1);
+    /** Math.random()が0以上であることを確認 */
+    expect(Math.random()).toBeGreaterThanOrEqual(0);
+  });
+
+  it("Date", () => {
+    const mockDate = new Date("2022-01-01");
+    /** 2022-01-01のDateモックを作成 */
+    const spy = jest
+      .spyOn(global, "Date")
+      .mockImplementation(() => mockDate as unknown as string);
+    /**今日のDateを取得しようとしても,mockDateが返される  */
+    expect(new Date()).toEqual(mockDate);
+    spy.mockRestore();
+  });
+
+  /** 引数の値を2倍にして返す関数のテスト */
+  it("double", () => {
+    /** dou */
+    const spy = jest.spyOn(calc, "double").mockImplementation(() => 4);
+    /** 本来  */
+    expect(calc.double(1)).toEqual(4);
+    spy.mockRestore();
+  });
+});
---
+++ src/modules/calc.ts
@@ -0,0 +1,9 @@
+const double = (value: number) => {
+  return value * 2;
+};
+
+const halve = (value: number) => {
+  return value / 2;
+};
+
+export { double, halve };
